5a9b44f260f69d80099be425bdde00cececa8730
p1613m <p1613m@gmail.com>
2023-12-25 11:51:58 +0500

    Add start game

---
+++ assets/js/drawable.js
@@ -0,0 +1,10 @@
+class Drawable {
+    constructor() {
+        this.$element = null
+        this.x = null
+        this.y = null
+        this.width = null
+        this.height = null
+        this.speed = null
+    }
+}
--- assets/js/game.js
+++ assets/js/game.js
@@ -1,27 +1,68 @@
 class Game {
     constructor() {
-        this.screens = {
+        this.$screens = {
             start: document.querySelector('.game-screen-start'),
             playground: document.querySelector('.game-screen-playground'),
             leaderboard: document.querySelector('.game-screen-leaderboard'),
         }
-        this.welcomeElements = {
+        this.$welcomeElements = {
             nickname: document.querySelector('.game-screen-start input'),
             button: document.querySelector('.game-screen-start button'),
         }
+        this.$controls = {
+            nickname: document.querySelector('#nickname'),
+            timer: document.querySelector('#timer'),
+            pause: document.querySelector('#pause'),
+        }
+
+        this.isStarted = false
+        this.nickname = null
+        this.seconds = 0
 
         this.binds()
     }
 
     binds() {
-        this.welcomeElements.nickname.addEventListener('input', () => {
-            const value = this.welcomeElements.nickname.value.trim()
-            if (value) {
-                this.welcomeElements.button.disabled = false
-            } else {
-                this.welcomeElements.button.disabled = true
-            }
+        this.$welcomeElements.nickname.addEventListener('input', () => {
+            const nickname = this.$welcomeElements.nickname.value.trim()
+            this.$welcomeElements.button.disabled = !nickname
         })
+
+        this.$welcomeElements.button.addEventListener('click', () => {
+            this.nickname = this.$welcomeElements.nickname.value.trim()
+            this.start()
+        })
+
+        this.$controls.pause.addEventListener('click', () => {
+            this.isStarted = !this.isStarted
+
+            this.$controls.pause.innerText = this.isStarted ? 'Pause' : 'Continue'
+        })
+    }
+
+    start() {
+        this.isStarted = true
+        this.$controls.nickname.innerText = this.nickname
+
+        setInterval(() => {
+            if (!this.isStarted) return;
+
+            this.seconds++
+            const minutes = ~~(this.seconds / 60)
+            const seconds = this.seconds % 60
+
+            this.$controls.timer.innerHTML = `${('0' + minutes).slice(-2)}:${('0' + seconds).slice(-2)}`
+        }, 1000)
+
+        this.changeScreen('playground')
+    }
+
+    changeScreen(screenName) {
+        for (const screenName in this.$screens) {
+            this.$screens[screenName].classList.remove('active')
+        }
+
+        this.$screens[screenName].classList.add('active')
     }
 }
 
